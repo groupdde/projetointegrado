36d0608f378f8960a793caed04a8f005d2402109
groupdde <136384017+groupdde@users.noreply.github.com>
2023-06-22 17:33:32 -0300

    Add files via upload

--- certificados.js
+++ certificados.js
@@ -13,10 +13,44 @@ function verMaisCertificados() {
   }
 }
 
-function exibirDiv() {
-  var div = document.getElementById('minhaDiv');
-  div.classList.toggle('escondida');
-}
-
 var maisCertificadosDiv = document.getElementById("maisCertificados");
 maisCertificadosDiv.classList.add("maiscertificadosescondido");
+
+var divAtualmenteExibida = null; 
+
+  function exibirDiv(numeroCertificado) {
+    var div = document.getElementById('minhaDiv');
+    var imagemCertificado = document.getElementById('imagemdiv');
+
+    if (divAtualmenteExibida === div) {
+      div.classList.add('escondida');
+      divAtualmenteExibida = null;
+      return;
+    }
+
+    if (divAtualmenteExibida !== null) {
+      divAtualmenteExibida.classList.add('escondida');
+    }
+
+    switch (numeroCertificado) {
+      case 1:
+        imagemCertificado.src = 'imagens/certificado1.PNG';
+        break;
+      case 2:
+        imagemCertificado.src = 'imagens/certificado2.PNG';
+        break;
+      case 3:
+        imagemCertificado.src = 'imagens/certificado3.PNG';
+        break;
+      case 4:
+        imagemCertificado.src = 'imagens/certificado4.PNG';
+        break;
+      default:
+
+        div.classList.add('escondida');
+        return;
+    }
+
+    div.classList.remove('escondida');
+    divAtualmenteExibida = div;
+  }
